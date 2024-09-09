f5ac569a5f46346bb00d80a0c765623f79ff6024
equinox1 <shep.antony@gmail.com>
2024-09-09 14:40:26 +0100

    main add

--- tsPyPackages/tsMqlML/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='tsMqlML',          # Required
-    version='0.0.1',           # Required
-    packages=find_packages(),  # Required
-)
