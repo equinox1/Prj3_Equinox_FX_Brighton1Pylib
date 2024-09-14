6b44be130e0dc23184fb81f5329ed0d85249095f
equinox1 <shep.antony@gmail.com>
2024-09-14 21:02:56 +0100

    pylibs

--- tsProjects/ExampleModel/tsPkgExample.py
+++ tsProjects/ExampleModel/tsPkgExample.py
@@ -10,7 +10,7 @@ print(mark.age)
 mark.age = 21
 print(mark.age) 
 """
-
+import MetaTrader5 as mt5
 from tsMqlConnect import CMqlinitdemo
 
 lp_path  =   r"c:\users\shepa\onedrive\8.0 projects\8.3 projectmodelsequinox\equinrun\mql5\brokers\icmarkets\terminal64.exe"
@@ -24,6 +24,6 @@ conn2 = CMqlinitdemo
 
 conn2.run_mql_login(lp_path,lp_login,lp_password,lp_server,lp_timeout,lp_portable)
 
-import MetaTrader5 as mt5
 
-print(" ASK:", mt5.TICK_FLAG_ASK)
+
+#print(" ASK:", mt5.TICK_FLAG_ASK)
--- tsProjects/prjNeuroPredict1/tsNeuroPredict.py
+++ tsProjects/prjNeuroPredict1/tsNeuroPredict.py
@@ -48,18 +48,21 @@ import tsMqlData
 import tsMqlML
 
 from tsMqlConnect import CMqlinitdemo
-from tsMqlData import CMqldatasetup
+from tsMqlData import CMqldatasetup 
 
+#======================================================
+# Login MQL Terminal
+#======================================================
+# Add to hash Vault
+lp_path  =   r"c:\users\shepa\onedrive\8.0 projects\8.3 projectmodelsequinox\equinrun\mql5\brokers\icmarkets\terminal64.exe"
+lp_login = 51698985
+lp_password =  r'lsor31tz$r8aih'
+lp_server = r"icmarketssc-demo"
+lp_timeout =  60000
+lp_portable = True
 
-ts1 = CMqlinitdemo
-#ts2 = CMqldatasetup
-
-ts1.set_mql_login()
-#run the program
-#lp_Obj_New=CMqlinitdemo
-#lp_Obj_New.set_mql_login(lp_path,lp_login,lp_password,lp_server,lp_timeout,lp_portable)
-
-
+conn1 = CMqlinitdemo
+conn1.run_mql_login(lp_path,lp_login,lp_password,lp_server,lp_timeout,lp_portable)
 
 """
 #+-------------------------------------------------------------------
