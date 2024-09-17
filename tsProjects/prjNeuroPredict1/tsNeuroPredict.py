333f92714eb32ef28b4299a7011dd5047b63ae1d
equinox1 <shep.antony@gmail.com>
2024-09-17 22:53:41 +0100

    creat neuro model

--- tsProjects/prjNeuroPredict1/tsNeuroPredict.py
+++ tsProjects/prjNeuroPredict1/tsNeuroPredict.py
@@ -88,64 +88,20 @@ mv_utc_from = d1.set_mql_timezone(mv_year,mv_month,mv_day,mv_timezone)
 print("Timezone Set to : ",mv_utc_from)
 mv_ticks1= pd.DataFrame(d1.run_load_from_mql(mv_debug,"mv_dfrates",mv_utc_from,mv_symbol_primary,mv_rows,mv_command))
 
-# Tabulate formatting
-# Use seaborn to set the style
-from tabulate import tabulate
-print(tabulate(mv_ticks1, showindex=False, headers=mv_ticks1.columns,tablefmt="grid",numalign="left",stralign="left",floatfmt=".4f"))
-print("Count: ",mv_ticks1.count())
-
 #+-------------------------------------------------------------------
 # Prepare Data
-#+-------------------------------------------------------------------  
-# This code converts the 'time' column to datetime format using seconds as the unit
-# and calculates the average of 'ask' and 'bid' values, assigning the result to a new
-# column named 'close' in the rates_frame DataFrame.The choice of input data for deep learning
-# in financial applications, such as predicting price movements, depends on various factors, and 
-# there isn't a one-size-fits-all solution. However, there are some considerations and general
-# guidelines to keep in mind:
-
-mv_ticks2=pd.DataFrame()
-# Empty DataFrame
+#+-------------------------------------------------------------------
+mv_ticks2=pd.DataFrame() # Empty DataFrame
 mv_ticks2 = mv_ticks2.drop(index=mv_ticks2.index)
 mv_ticks2 = pd.DataFrame(mv_ticks1)
-#To simplify things, let's add and divide the bid and ask values in the data by two.
-# This way, we obtain the average value, which will be our input for deep learning.
-mv_ticks2['time']=pd.to_datetime(mv_ticks2['time'], unit='s')
-mv_ticks2['close']=(mv_ticks2['ask']+mv_ticks2['bid'])/2
 
 #+-------------------------------------------------------------------
 # Shift Data
 #+-------------------------------------------------------------------  
-#Shifting a DataFrame in the context of deep learning, particularly in time series forecasting,
-#is commonly done to create sequences of input and target variables. Here are the reasons why shifting
-# is used in the context of deep learning for time series prediction:
-#Temporal Dependencies: Deep learning models, such as recurrent neural networks (RNNs)
-# or long short-term memory networks (LSTMs), can capture temporal dependencies in sequential data. 
-# Shifting the DataFrame allows you to create sequences where each input sequence corresponds to a segment of past data
-# and the corresponding target sequence represents the future data.
-# Sequential Learning: Deep learning models are effective at learning patterns and dependencies in sequential data. 
-# By shifting the DataFrame, you ensure that the input and target sequences align temporally, 
-# allowing the model to learn from the historical context and make predictions based on that context.
-# Training Input-Output Pairs: Shifting helps in creating training examples for the deep learning model. 
-# Each row in the shifted DataFrame can be considered an input-output pair, where the input is 
-# a sequence of past observations, and the output is the target variable to be predicted in the future.
-
-#number_of_rows is a variable representing the number of seconds.
-#empty_rows creates a new DataFrame with NaN values, having the same columns as the original DataFrame ( df ).
-#df.append(empty_rows, ignore_index=True) appends the empty rows to the original DataFrame ( df ) 
-#while ignoring the index to ensure a continuous index.
-#df['target'] = df['close'].shift(-seconds) creates a new column 'target' containing the 'close' values 
-# shifted by a negative value of the specified number of seconds. 
-# This is commonly done when preparing time series data for predictive modeling.
-
-mv_seconds =60
-mv_number_of_rows = mv_seconds 
-
-empty_rows = pd.DataFrame(np.nan, index=range(mv_number_of_rows), columns=mv_ticks2.columns)
-
-mv_ticks2 = mv_ticks2._append(empty_rows, ignore_index=True)
-mv_ticks2['target'] = mv_ticks2['close'].shift(-mv_seconds)
-#The result is a modified DataFrame ( df ) with additional rows filled with NaN values and a new 'target' column for time-shifted 'close' values.
-mv_ticks2=mv_ticks2.dropna()
-print("======================DF Modified:=================================")
-print(tabulate(mv_ticks2, showindex=False, headers=mv_ticks1.columns,tablefmt="grid",numalign="left",stralign="left",floatfmt=".4f"))
+# Tabulate formatting
+# Use seaborn to set the style
+from tabulate import tabulate
+lp_seconds = 60
+lp_unit = 's' 
+mv_ticks3 = d1.run_shift_data1(mv_ticks2, lp_seconds,lp_unit)
+print(tabulate(mv_ticks3, showindex=False, headers=mv_ticks1.columns,tablefmt="pretty",numalign="left",stralign="left",floatfmt=".4f"))
--- tsPyPackages/tsMqlData/tsMqlData.py
+++ tsPyPackages/tsMqlData/tsMqlData.py
@@ -44,6 +44,7 @@ import MetaTrader5 as mt5
 import pandas as pd
 import pytz
 from datetime import datetime
+import numpy as np
 
 #+-------------------------------------------------------------------
 # import ai packages scikit learns
@@ -210,85 +211,34 @@ class CMqldatasetup:
         lp_rates1 = mt5.copy_ticks_from(lp_symbol, lp_utc_from, lp_rows, lp_flag )
         print("ticks received:",len(lp_rates1))
         return lp_rates1
+    
+#--------------------------------------------------------------------
+# create method  "run_shift_data1()".
+# class: cmqldatasetup      
+# usage: mql data
+# \param  var                          
+#--------------------------------------------------------------------         
 
-#--------------------------------------------------------------------
-# create method  "set_load_from_mql_toframe()".
-# class: cmqldatasetup      
-# usage: mql data
-# \param  var                          
-#--------------------------------------------------------------------     
-    def set_load_from_mql_toframe(lp_rates1 = "rates" ,lp_rates2 = "rates_frame"):
-        # copy rates into a new panda dataframe
-        lv_rates2=pd.dataframe()
-        lv_rates2 = lv_rates2.drop(index=lp_rates2.index)
-        lv_rates2 = pd.dataframe(lp_rates1)
-        #add and divide the bid and ask values in the data by two. this way, we obtain the average value, which will be our input for deep learning.
-        lv_rates2['time']=pd.to_datetime(lv_rates2['time'], unit='s')
-        lv_rates2['close']=(lv_rates2['ask']+lv_rates2['bid'])/2
-        return lv_rates2
-
-#-------------------------------------------------------
-# create method  "set_data_frame_shift()".
-# class: cmqldatasetup      
-# usage: mql data
-# \param  var                          
-#-------------------------------------------------------------------- 
-    def set_data_frame_shift(lp_seconds = 60, lp_rates2 = "rates_frame"):
-        #shifting a dataframe in time series forecasting, is commonly done to create sequences of input and target variables
-        #number_of_rows is a variable representing the number of seconds.
-        #empty_rows creates a new dataframe with nan values, having the same columns as the original dataframe ( rates_frame).
-        number_of_rows=lp_seconds
-        empty_rows = pd.dataframe(np.nan, index=range(number_of_rows), columns=lp_rates2.columns)
-        #if.append(empty_rows, ignore_index=true) appends the empty rows to the original dataframe ( df ) while ignoring the index to ensure a continuous index.
-        lp_rates2= lp_rates2._append(empty_rows, ignore_index=True)
-        #df['target'] = df['close'].shift(-seconds) creates a new column 'target' containing the 'close' values shifted by a negative value of the specified number of seconds. this is commonly done when preparing time series data for predictive modeling.
-        lp_rates2['target'] = lp_rates2['close'].shift(-lp_seconds)
-        print("rates_frame modified",lp_rates2)
-        return lp_rates2
-#--------------------------------------------------------------------
-# class: cmqldatasetup      
-# usage: mql data
-# \param  var                          
-#-------------------------------------------------------------------- 
-    def set_data_frame_shift_clean(lp_rates2):
-        #now, all that's left is to clean the data so that we can use it as input with tensorflow.
-        lp_rates2=lp_rates2.dropna()
-        #the result is a modified dataframe ( df ) with additional rows filled with nan values and 
-        # a new 'target' column for time-shifted 'close' values.
-        return lp_rates2
-#--------------------------------------------------------------------
-# create method  "set_mql_newdf_step()".
-# class: cmqldatasetup      
-# usage: mql data
-# \param  var                          
-#--------------------------------------------------------------------     
-    def set_mql_newdf_step(lp_target) :
-        lv_new_df = pd.DataFrame({'Open': lp_target['Open'],'High': lp_target['High'],'Low': lp_target['Low'],'Close': lp_target['Close'] })
-        return lv_new_df     
-#--------------------------------------------------------------------   
-# create method  "set_mql_target_step()".
-# class: cmqldatasetup      
-# usage: mql data
-# \param  var 
-#--------------------------------------------------------------------                         
-    def set_mql_target_step(lp_df,lp_future = 10):
-        lv_target = pd.concat([lp_df['close'].shift(-i) for i in range(1, lp_future + 1)], axis=1) # using close prices for the next i
-        lv_target.columns = [f'target_close_{i}' for i in range(1, lp_future + 1)] # naming the columns
-        return lv_target  
-#--------------------------------------------------------------------     
-# usage: This function creates the new target variable using the column "Signal" from the dataset.
-# usage: The target variable is taken from the index step+1 value in the signal column
-# usage:  At step 1, the next signal will be 2, at step 2,  the next signal will be 3, and so on.
-# usage: This means that in step 1, we create a dataset in such a way the target variable is a value from 1
-# usage: bar in the future, and so on. All the independent variables remain the same.
-# \param  var                          
-#--------------------------------------------------------------------   
-    def multi_steps_data_process(lp_dfmql, lp_step, lp_train_size=0.7, lp_random_state=42):
-        # since we are using the open high low close ohlc values only
-        lp_dfmql["next signal"] = lp_dfmql["signal"].shift(-lp_step) # the target variable from next n future values
-        #lp_dfmql = lp_dfmql.dropna()
-        #y = lp_dfmql["next signal"]
-        #x = lp_dfmql.drop(columns=["signal", "next signal"])
-        #lv_result=train_test_split(x, y, train_size=lp_train_size, random_state=lp_random_state) 
-        lv_result = 1
-        return lv_result
+    def run_shift_data1(lp_df, lp_seconds = 60 , lp_unit = 's'):
+        
+        lv_seconds =lp_seconds
+        lv_number_of_rows = lv_seconds 
+        #+-------------------------------------------------------------------  
+        # This code converts the 'time' column to datetime format using seconds as the unit
+        # and calculates the average of 'ask' and 'bid' values, assigning the result to a new
+        # column named 'close' in the rates_frame DataFrame. lp_unit 's' = seconds
+        #+------------------------------------------------------------------- 
+        # head time(datetime),bid(float5),ask(float5),last  volume(int),time_msc(datetime),flags(int),volume_real(int),close(float5),target(float5) 
+        lp_df.style.set_properties(**{'text-align': 'left'})
+        #lp_df['time']=pd.to_datetime(lp_df['time'], unit=lp_unit)
+        lp_df['time']= pd.to_datetime(lp_df.time, format='%Y%m%d%H%M%S', errors='coerce')
+        #lp_df['time_msc']= pd.to_datetime(lp_df.time_msc, format='%Y%m%d%H%M%S', errors='coerce')    
+        
+        lp_df['close']=(lp_df['ask']+lp_df['bid'])/2
+        
+        lv_empty_rows = pd.DataFrame(np.nan, index=range(lv_number_of_rows), columns=lp_df.columns)
+        lp_df = lp_df._append(lv_empty_rows, ignore_index=True)
+        lp_df['target'] = lp_df['close'].shift(-lv_seconds)
+        lp_df=lp_df.dropna()
+        lp_df.style.set_properties(**{'text-align': 'left'})
+        return lp_df
