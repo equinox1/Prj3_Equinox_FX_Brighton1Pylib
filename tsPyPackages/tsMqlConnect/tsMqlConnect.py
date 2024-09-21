c82bd13930958e03fb7a893c0ce14a18f7ddb3f0
equinox1 <shep.antony@gmail.com>
2024-09-21 22:48:59 +0100

    midtensor

--- tsPyPackages/tsMqlConnect/tsMqlConnect.py
+++ tsPyPackages/tsMqlConnect/tsMqlConnect.py
@@ -16,7 +16,7 @@ The PEP 8 Style Guide provides guidelines for naming convent
 Naming conventions:
 ==================
 Variables, functions, and methods should be named in lowerca
-Class names should be written in CamelCase (starting with a 
+Class names should be written in CamelCase (starting with a
 Constants should be written in all capital letters with unde
 
 Indentation:
@@ -51,7 +51,7 @@ import MetaTrader5 as mt5
 #
 # section:params
 # \param  double svar;              -  value
-#-------------------------------------------------------------------- 
+#--------------------------------------------------------------------
 class CMqlinit:
     def __init__(self,lp_path,lp_login,lp_password,lp_server,lp_timeout,lp_portable):
         self._lp_path  =  lp_path
@@ -60,8 +60,8 @@ class CMqlinit:
         self._lp_server = lp_server
         self._lp_timeout = lp_timeout
         self._lp_portable = lp_portable
-    
-    #Setter and Getter lp_path  
+
+    #Setter and Getter lp_path
     @property
     def lp_path(self):
         return self.lp_path
@@ -69,65 +69,71 @@ class CMqlinit:
     @lp_path.setter
     def lp_path(self, value):
         self._lp_path = value.encode("utf-8")
-        
-    #Setter and Getter lp_login  
+
+    #Setter and Getter lp_login
     @property
     def lp_login(self):
         return self.lp_login
-    
+
     @lp_login.setter
     def lp_login(self, value):
         self._lp_login = value
-        
+
     #Setter and Getter lp_password
     @property
     def lp_password(self):
         return self.lp_password
-    
+
     @lp_password.setter
     def lp_password(self, value):
         self._lp_password = value
-        
+
     #Setter and Getter lp_server
     @property
     def lp_server(self):
         return self.lp_server
-    
+
     @lp_server.setter
     def lp_server(self,value):
         self._lp_server = value
-        
+
     #Setter and Getter lp_timeout
     @property
     def lp_timeout(self):
         return self.lp_timeout
-    
+
     @lp_timeout.setter
     def lp_timeout(self,value):
-        self._lp_timeout = value 
-        
+        self._lp_timeout = value
+
     #Setter and Getter lp_portable
     @property
     def lp_portable(self):
         return self.lp_portable
-    
+
     @lp_portable.setter
     def lp_portable(self,value):
-        self._lp_portable = value         
-        
+        self._lp_portable = value
 #--------------------------------------------------------------------
 # create method  "run_mql_login()".
-# class: cmqlinit      
+# class: cmqlinit
 # usage: login
-# \param cmqlinit    var                          
+# \param cmqlinit    var
 #--------------------------------------------------------------------
-    def run_mql_login(lp_path,lp_login,lp_password,lp_server,lp_timeout,lp_portable):
-        if mt5.initialize(path=lp_path,login=lp_login,password=lp_password,server=lp_server,timeout=lp_timeout,portable=lp_portable):
+    def run_mql_login(self, lp_login, lp_password, lp_server, lp_timeout, lp_portable):
+        if mt5.initialize(
+            path=self,
+            login=lp_login,
+            password=lp_password,
+            server=lp_server,
+            timeout=lp_timeout,
+            portable=lp_portable,
+        ):
             print("Platform mt5 launched correctly")
-            lp_return=mt5.last_error()
         else:
             print(f"there has been a problem with initialization: {mt5.last_error()}")
-            lp_return=mt5.last_error()
+
+        lp_return=mt5.last_error()
 
 #--------------------------------------------------------------------
 # create class  "CMqlinitdemo"
@@ -135,7 +141,7 @@ class CMqlinit:
 #
 # section:params
 # \param  double svar;              -  value
-#-------------------------------------------------------------------- 
+#--------------------------------------------------------------------
 class CMqlinitdemo(CMqlinit):
     pass
 
@@ -145,6 +151,6 @@ class CMqlinitdemo(CMqlinit):
 #
 # section:params
 # \param  double svar;              -  value
-#-------------------------------------------------------------------- 
+#--------------------------------------------------------------------
 class CMqlinitprod(CMqlinit):
     pass
--- tsPyPackages/tsMqlML/tsMqlML.py
+++ tsPyPackages/tsMqlML/tsMqlML.py
@@ -11,37 +11,54 @@
 #+-------------------------------------------------------------------
 import numpy as np
 import pandas as pd
+
+
 #+-------------------------------------------------------------------
-# import ai packages scikit learns
+# import ai packages scikit-learns
 #+-------------------------------------------------------------------
-#sklearn library. it includes utilities for data preprocessing, model evaluation, and model
 from sklearn import datasets, svm, metrics
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-#This line imports the KFold class from scikit-learn, which is often used for cross-validat
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 
 #======================================================
 # import ai packages tensorflow and keras libraries
-#====================================================== 
-import tensorflow as tf; tf.keras
-from tensorflow.python import keras 
+#======================================================
+import tensorflow as tf
+import keras
+from keras import Sequential
+from keras import layers
+from keras import ops
+
+from tensorflow.python.keras import layers
 
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.regularizers import l2
-
-from tensorflow.python.keras.engine import data_adapter
-
 #-----------------------------------------------
 # Class CMqlmlsetup
 #+--------------------------------------------------
 class CMqlmlsetup:
-    def __init__(self, lp_df ,lv_X = [], lv_y = [] ,lv_X_train = [], lv_X_train_scaled =[],lv_X_test =[], lv_X_test_scaled = [],lv_y_train =[],lv_y_test = []):
+    def __init__(self, lp_df, lv_X=None, lv_y=None, lv_X_train=None, lv_X_train_scaled=None, lv_X_test=None, lv_X_test_scaled=None, lv_y_train=None, lv_y_test=None):
+        if lv_X is None:
+            lv_X = []
+        if lv_y is None:
+            lv_y = []
+        if lv_X_train is None:
+            lv_X_train = []
+        if lv_X_train_scaled is None:
+            lv_X_train_scaled = []
+        if lv_X_test is None:
+            lv_X_test = []
+        if lv_X_test_scaled is None:
+            lv_X_test_scaled = []
+        if lv_y_train is None:
+            lv_y_train = []
+        if lv_y_test is None:
+            lv_y_test = []
         self._lp_df = lp_df
-        
     @property
     def lp_df(self):
         return self.lp_df
@@ -49,7 +66,6 @@ class CMqlmlsetup:
     @lp_df.setter
     def lp_dfx(self, value):
         self._lp_df = value
-                
     @property
     def lv_X(self):
         return self.lv_X
@@ -57,7 +73,6 @@ class CMqlmlsetup:
     def lv_X(self, value):
         value = self._lv_X = []
         self._lv_X = value
-        
     @property
     def lv_y(self):
         return self.lv_y
@@ -65,31 +80,28 @@ class CMqlmlsetup:
     def lv_y(self, value):
         value = self._lv_y = []
         self._lv_y = value
-        
     @property
     def lv_X_train(self):
         return self.lv_X_train
     @lv_X_train.setter
     def lv_X_train(self, value):
-        self._lv_X_train = value   
-        
+        self._lv_X_train = value
     @property
     def lv_X_test(self):
         return self.lv_X_test
     @lv_X_test.setter
     def lv_X_test(self, value):
-        self._lv_X_test = value      
-        
+        self._lv_X_test = value
     @property
     def lv_X_train_scaled(self):
         return self.lv_X_train_scaled
     @lv_X_train_scaled.setter
-    
+
     def lv_X_train_scaled(self, value):
         scaler = StandardScaler()
         value = scaler.fit_transform(self.lv_X_train)
-        self._lv_X_train_scaled = value   
-        
+        self._lv_X_train_scaled = value
+
     @property
     def lv_X_test_scaled(self):
         return self.lv_X_test_scaled
@@ -97,112 +109,126 @@ class CMqlmlsetup:
     def lv_X_test_scaled(self, value):
         scaler = StandardScaler()
         value = scaler.transform(self.lv_X_test)
-        self._lv_X_test_scaled = value          
-        
+        self._lv_X_test_scaled = value
+
     @property
     def lv_y_train(self):
         return self.lv_y_train
     @lv_y_train.setter
     def lv_y_train(self, value):
-        self._lv_y_train = value       
+        self._lv_y_train = value
 
     @property
     def lv_y_test(self):
         return self.lv_y_test
     @lv_y_test.setter
     def lv_y_test(self, value):
-        self._lv_y_test = value  
-        
-    def _is_distributed_dataset(ds):
-        return isinstance(ds, data_adapter.input_lib.DistributedDatasetSpec)
-
-    data_adapter._is_distributed_dataset = _is_distributed_dataset
-        
-        
+        self._lv_y_test = value
 #--------------------------------------------------------------------
 # create method  "dl_split_data_sets".
-# class:cmqlmlsetup      
+# class:cmqlmlsetup
 # usage: mql data
 # \pdlsplit data
 #--------------------------------------------------------------------
-    def dl_split_data_sets(lp_df, lv_X = [],lv_y = [],lp_test_size=0.2,lp_shuffle = False, lp_prog = 1): 
-        lv_X = lp_df[['close']]   
+    def dl_split_data_sets(self, lp_df, lv_X=None, lv_y=None, lp_test_size=0.2, lp_shuffle = False, lp_prog = 1):
+        if lv_X is None:
+            lv_X = []
+        if lv_y is None:
+            lv_y = []
+        lv_X = lp_df[['close']]
         lv_y = lp_df['target']
-    
-        lv_X_train,lv_y_train,lv_X_test,lv_y_test =  train_test_split(lv_X, lv_y, test_size=lp_test_size, shuffle=lp_shuffle)        
+
+        lv_X_train,lv_y_train,lv_X_test,lv_y_test =  train_test_split(lv_X, lv_y, test_size=lp_test_size, shuffle=lp_shuffle)
         if lp_prog == 1:
             return lv_X_train
         if lp_prog == 2:
-            return lv_y_train     
-        if lp_prog == 3:          
+            return lv_y_train
+        if lp_prog == 3:
             return lv_X_test
         if lp_prog == 4:
-            return lv_y_test            
-#--------------------------------------------------------------------       
+            return lv_y_test
+#--------------------------------------------------------------------
 # create method  "dl_train_model_scaled".
-# class:cmqlmlsetup      
+# class:cmqlmlsetup
 # usage: mql data
-# \param  var  
-#-------------------------------------------------------------------- 
-    def dl_train_model_scaled(lp_X_train):
+# \param  var
+#--------------------------------------------------------------------
+    def dl_train_model_scaled(self):
         # meta names
         scaler = StandardScaler()
-        lp_X_train_scaled = scaler.fit_transform(lp_X_train)
-        return lp_X_train_scaled
-    
-#-------------------------------------------------------------------- 
+        return scaler.fit_transform(self)
+
+#--------------------------------------------------------------------
 # create method  "dl_test_model_scaled".
-# class:cmqlmlsetup      
+# class:cmqlmlsetup
 # usage: mql data
-# \param  var  
-#----------test----------------------------------------------------- 
-    def dl_test_model_scaled(lp_X_test = []):
+# \param  var
+#----------test----------------------------------------------------
+    def dl_test_model_scaled(self):
         # meta names
         scaler = StandardScaler()
-        lp_X_test_scaled = scaler.fit_transform(lp_X_test)
-        return lp_X_test_scaled   
-        
+        return scaler.fit_transform(self)
+
 
 #--------------------------------------------------------------------
 # create method  "dl_build_neuro_network".
-# class:cmqlmlsetup      
+# class: cmqlmlsetup
 # usage: mql data
 # \pdl_build_neuro_network
 #--------------------------------------------------------------------
-    def dl_build_neuro_network(lp_k_reg= 0.001,lp_X_train = [] ,lp_optimizer = 'adam',lp_loss = 'mean_squared_error'):
+    def dl_build_neuro_network(self, lp_X_train=None, lp_optimizer = 'adam', lp_loss = 'mean_squared_error'):
+        if lp_X_train is None:
+            lp_X_train = []
         # Build a neural network model
         model=None
         model = Sequential()
-        model.add(Dense(128, activation='relu', input_shape=(lp_X_train.shape[1],), kernel_regularizer=l2(lp_k_reg)))
-        model.add(Dense(256, activation='relu', kernel_regularizer=l2(lp_k_reg)))
-        model.add(Dense(128, activation='relu', kernel_regularizer=l2(lp_k_reg)))
-        model.add(Dense(64, activation='relu', kernel_regularizer=l2(lp_k_reg)))
+        model.add(
+                Dense(
+                128,
+                activation='relu',
+                input_shape=(lp_X_train.shape[1],),
+                kernel_regularizer=l2(self),
+            )
+        )
+        model.add(Dense(256, activation='relu', kernel_regularizer=l2(self)))
+        model.add(Dense(128, activation='relu', kernel_regularizer=l2(self)))
+        model.add(Dense(64, activation='relu', kernel_regularizer=l2(self)))
         model.add(Dense(1, activation='linear'))
         model.compile(optimizer='adam', loss='mean_squared_error')
-        #model=model.compile(optimizer=lp_optimizer, loss=lp_loss)  
+        #model=model.compile(optimizer=lp_optimizer, loss=lp_loss)
         return model
-    
+
 
 # create method  "dl_train_model".
-# class:cmqlmlsetup      
-# usage: mql data
-# \param  var  
-#-------------------------------------------------------------------- 
+# class:cmqlmlsetup  usage: mql data
+# \param  var
+#--------------------------------------------------------------------
 
-    def dl_train_model(lp_model = [],lp_X_train_scaled = [],lp_y_train = [],lp_epoch = 1,lp_batch_size = 256, lp_validation_split = 0.2,lp_verbose =1):
+    def dl_train_model(self, lp_X_train_scaled=None, lp_y_train=None, lp_epoch = 1, lp_batch_size = 256, lp_validation_split = 0.2, lp_verbose =1):
+        if lp_X_train_scaled is None:
+            lp_X_train_scaled = []
+        if lp_y_train is None:
+            lp_y_train = []
         lp_X_train_scaled = tf.stack(lp_X_train_scaled)
         lp_y_train = tf.stack(lp_y_train)
-        lp_model.fit(lp_X_train_scaled, lp_y_train, epochs=lp_epoch, batch_size=lp_batch_size, validation_split=lp_validation_split, verbose=lp_verbose)
-        return lp_model
+        self.fit(
+            lp_X_train_scaled,
+            lp_y_train,
+            epochs=lp_epoch,
+            batch_size=lp_batch_size,
+            validation_split=lp_validation_split,
+            verbose=lp_verbose,
+        )
+        return self
 
 
 #--------------------------------------------------------------------
 # create method  "dl_predict_network"
-# class:cmqlmlsetup      
+# class:cmqlmlsetup
 # usage: mql data
 # \pdl_build_neuro_network
 #--------------------------------------------------------------------
-    def dl_predict_values(lp_df, lp_model,lp_seconds = 60):         
+    def dl_predict_values(self, lp_model, lp_seconds = 60):
         # Use the model to predict the next N instances
         lv_X_predict=[]
         lv_X_predict_scaled=[]
@@ -211,35 +237,34 @@ class CMqlmlsetup:
         lv_predictions=[]
         scaler = StandardScaler()
         # Empty DataFrame
-        lv_X_predict = lp_df.tail(lp_seconds)[['close']]
+        lv_X_predict = self.tail(lp_seconds)[['close']]
         lv_X_predict_scaled = scaler.transform(lv_X_predict)
-        lv_predictions = lp_model.predict(lv_X_predict_scaled)
-        return lv_predictions
+        return lp_model.predict(lv_X_predict_scaled)
 
 #--------------------------------------------------------------------
 # create method  "dl_model_performance"
-# class:cmqlmlsetup      
+# class:cmqlmlsetup
 # usage: mql data
 # \pdl_build_neuro_network
 #--------------------------------------------------------------------
-#Mean Squared Error (MSE): It measures the average squared difference between the predicted and actual values. 
+#Mean Squared Error (MSE): It measures the average squared difference between the predicted and actual values.
 # The lower the MSE, the better the model.
 
-#Mean Absolute Error (MAE): It measures the average absolute difference between the predicted and actual values. 
+#Mean Absolute Error (MAE): It measures the average absolute difference between the predicted and actual values.
 # Like MSE, lower values indicate better model performance.
 
-#R2 Score: Also known as the coefficient of determination, it measures the proportion of the variance in the 
-# dependent variable that is predictable from the independent variable(s). An R2 score of 1 indicates 
+#R2 Score: Also known as the coefficient of determination, it measures the proportion of the variance in the
+# dependent variable that is predictable from the independent variable(s). An R2 score of 1 indicates
 # a perfect fit, while a score of 0 suggests that the model is no better than predicting the mean of the
 #target variable. Negative values indicate poor model performance.
-    def dl_model_performance(lp_y_test,lp_model,lp_X_test_scaled):
+    def dl_model_performance(self, lp_model, lp_X_test_scaled):
         # Calculate and print mean squared error
-        mse = mean_squared_error(lp_y_test, lp_model.predict(lp_X_test_scaled))
+        mse = mean_squared_error(self, lp_model.predict(lp_X_test_scaled))
         print(f"\nMean Squared Error: {mse}")
         # Calculate and print mean absolute error
-        mae = mean_absolute_error(lp_y_test, lp_model.predict(lp_X_test_scaled))
+        mae = mean_absolute_error(self, lp_model.predict(lp_X_test_scaled))
         print(f"\nMean Absolute Error: {mae}")
         # Calculate and print R2 Score
-        r2 = r2_score(lp_y_test, lp_model.predict(lp_X_test_scaled))
+        r2 = r2_score(self, lp_model.predict(lp_X_test_scaled))
         print(f"\nR2 Score: {r2}")
         return r2
