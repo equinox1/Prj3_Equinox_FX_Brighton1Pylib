4892ada4c339bec01a06db36065c08d41678eb61
equinox1 <shep.antony@gmail.com>
2024-10-17 01:09:43 +0100

    baseprecgpt

--- tsProjects/prjNeuroPredict1/tsNeuroPredict.py
+++ tsProjects/prjNeuroPredict1/tsNeuroPredict.py
@@ -226,6 +226,11 @@ mt = CMdtuner(mp_X_train_input_shape,
                mp_epochs,
                mp_batch_size,
                mp_arraysize,
+               mp_channels,
+               mp_lstm_shape,
+               mp_cnn_shape,
+               mp_gru_shape,
+               mp_transformer_shape,
                )
 
 # Run the tuner to find the best model configuration
