79c29582c1481c80c1bef57a98df4aee09ca5b7b
equinox1 <shep.antony@gmail.com>
2024-11-28 19:33:59 +0000

    upd

--- tsProjects/Keyscript/key.py
+++
@@ -1,9 +0,0 @@
-
-import keyring as kr
-kr.set_password("xerces_icm", "51698985", r"LsOR31tz$r8AIH") 
-kr.set_password("xerces_meta", "5031861048", r"JyMj!o5g") 
-
-kr.get_password("xerces_icm", "51698985")
-kr.get_password("xerces_meta", "5031861048")
-print(kr.get_password("xerces_icm", "51698985"))
-print(kr.get_password("xerces_meta", "5031861048"))
