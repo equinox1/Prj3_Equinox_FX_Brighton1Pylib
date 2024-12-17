c5041f2ba9cbb309f6110edf3babfe66016ce6c4
equinox1 <shep.antony@gmail.com>
2024-12-17 12:51:28 +0000

    dataloads

--- tsPyPackages/tsMqlData/tsMqlData.py
+++ tsPyPackages/tsMqlData/tsMqlData.py
@@ -27,7 +27,8 @@ from sklearn.preprocessing import MinMaxScaler
 #-------------------------------------------------------------------- 
 class CMqldatasetup:
     def __init__(self, **kwargs):
-        self.mv_loadApi = kwargs.get('mv_loadApi', False)
+        self.mv_loadapiticks = kwargs.get('mv_loadApi', False)
+        self.mv_loadapirates = kwargs.get('mv_loadApiRates', False)
         self.mv_loadFile = kwargs.get('mv_loadFile', False)
         self.mp_dfName = kwargs.get('mp_dfName', None)
         self.mv_utc_from = kwargs.get('mv_utc_from', None)
@@ -52,9 +53,8 @@ class CMqldatasetup:
         self.lp_path = kwargs.get('lp_path', '.')
         self.lp_filename = kwargs.get('lp_filename', 'tickdata1')
         self.lp_seconds = kwargs.get('lp_seconds', None)
-        self.svar = kwargs.get('svar', 0.0)
-
-
+        self.lp_timeframe = kwargs.get('lp_timeframe', 'mt5.TIMEFRAME_M1')
+       
 
     # create method  "setmql_timezone()".
     # class: cmqldatasetup      
@@ -69,44 +69,71 @@ class CMqldatasetup:
     # class: cmqldatasetup      
     # usage: mql data
     # /param  var                          
-    def run_load_from_mql(self, lp_loadapi,lp_loadfile, lp_rates1,lp_rates2, lp_utc_from, lp_symbol, lp_rows, lp_rowcount, lp_command, lp_path, lp_filename):
+    def run_load_from_mql(self, lp_loadapiticks ,lp_loadapirates,lp_loadfileticks,lp_loadfilerates, lp_rates1,lp_rates2, lp_utc_from, lp_symbol, lp_rows, lp_rowcount, lp_command, lp_path, lp_filename1,lp_filename2,lp_timeframe):
         # request 100 000 eurusd ticks starting from lp_year, lp_month, lp_day in utc time zone
-        print("lp_loadapi:", lp_loadapi)
-        print("lp_loadfile:", lp_loadfile)
-        print("lp_rates1:", lp_rates1)
-        print("lp_utc_from:", lp_utc_from)
-        print("lp_symbol:", lp_symbol)
-        print("lp_rows:", lp_rows)
-        print("lp_command:", lp_command)
+        
         lp_rates1 = pd.DataFrame()
         lp_rates1 = lp_rates1.drop(index=lp_rates1.index)
         lp_rates2 = pd.DataFrame()
         lp_rates2 = lp_rates2.drop(index=lp_rates2.index)
+        lp_rates3 = pd.DataFrame()
+        lp_rates3 = lp_rates3.drop(index=lp_rates3.index)
 
-        if lp_loadapi:
-            print("mt5 version:", mt5.version())
-            print("mt5 info:", mt5.terminal_info())
+        if lp_loadapiticks:
             try:
-                lp_rates1 = mt5.copy_ticks_from(lp_symbol, lp_utc_from, lp_rows, lp_flag)
+                #copy_ticks_from(
+                #symbol,       // symbol name
+                #date_from,    // date the ticks are requested from
+                #count,        // number of requested ticks
+                #flags         // combination of flags defining the type of requested ticks
+                #)
+                lp_rates1 = mt5.copy_ticks_from(lp_symbol, lp_utc_from, lp_rows, lp_command)
                 lp_rates1 = pd.DataFrame(lp_rates1)
-                print("2:lp_rates1 loaded via api:", lp_rates1.head())
+                print(" Api tick data received:", len(lp_rates1))
                 if lp_rates1 is None or len(lp_rates1) == 0:
                     print("1:No tick data found")  
             except Exception as e:
                 e = mt5.last_error()
                 print(f"Mt5 result: {e}")
 
-        if lp_loadfile:    
-            lpmergepath = lp_path + "//" + lp_filename
-            print("3:Manual load_filename:", lpmergepath)
+        if lp_loadapirates:
+            try:
+                #copy_rates_from(
+                #symbol,       // symbol name
+                #timeframe,    // timeframe
+                #date_from,    // initial bar open date
+                #count         // number of bars
+                #)
+                lp_rates2 = mt5.copy_rates_from(lp_symbol,lp_timeframe ,lp_utc_from, lp_rows)
+                lp_rates2 = pd.DataFrame(lp_rates2)
+                print(" Api rates data received:", len(lp_rates2))
+                if lp_rates2 is None or len(lp_rates2) == 0:
+                    print("1:No rate data found")  
+            except Exception as e:
+                e = mt5.last_error()
+                print(f"Mt5 result: {e}")
+
+        if lp_loadfileticks:    
+            lpmergepath = lp_path + "//" + lp_filename1
             
             try:
-                lp_rates2 = pd.read_csv(lpmergepath, sep=',', nrows=lp_rowcount)
-                print("3:ticks received:", len(lp_rates2))
+                lp_rates3 = pd.read_csv(lpmergepath, sep=',', nrows=lp_rowcount)
+                print("File tick data received:", len(lp_rates3))
             except Exception as e:
                 e = mt5.last_error()
                 print(f"Fileload result: {e}")
-        return lp_rates1 , lp_rates2
+
+        if lp_loadfilerates:    
+            lpmergepath = lp_path + "//" + lp_filename2
+            
+            try:
+                lp_rates4 = pd.read_csv(lpmergepath, sep=',', nrows=lp_rowcount)
+                print("File rates data received:", len(lp_rates4))
+            except Exception as e:
+                e = mt5.last_error()
+                print(f"Fileload result: {e}")
+
+        return lp_rates1 , lp_rates2, lp_rates3, lp_rates4
 
 
     # create method  "run_shift_data1()".
