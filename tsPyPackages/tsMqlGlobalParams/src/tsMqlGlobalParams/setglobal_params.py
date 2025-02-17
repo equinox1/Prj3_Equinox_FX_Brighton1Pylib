8b71c5a7c63acf931f47d9ad6b51c53b9af6da0f
equinox1 <shep.antony@gmail.com>
2025-02-17 23:23:31 +0000

    optomised env

--- tsPyPackages/tsMqlGlobalParams/src/tsMqlGlobalParams/setglobal_params.py
+++ tsPyPackages/tsMqlGlobalParams/src/tsMqlGlobalParams/setglobal_params.py
@@ -11,8 +11,9 @@ loadmql=pchk.check_mql_state()
 logger.info(f"Running on: {os_platform} and loadmql state is {loadmql}")
 
 from tsMqlDataParams import CMqlEnvData
-from tsMqlML import CMqlmlsetup, CMqlEnvML
-from tsMqlMLTuneParams import CMdtunerHyperModel, CMqlEnvTuneML
+from tsMqlMLParams import CMqlEnvML
+from tsMqlTuneMLParams import CMqlEnvTuneML
+from tsMqlModelParams import CMdtunerHyperModel
 
 
 class CMqlEnvGlobal:
@@ -20,6 +21,12 @@ class CMqlEnvGlobal:
         
         self.kwargs = kwargs
         self.config = config  # Store config for later use
+        self.config_platform = config.get("default_platform", None) # equinrun for Windows, equinrunlin for Linux, equinrunmac for MacOS
+        logger.info(f"Setting up global environment for platform: {self.config_platform}")
+         # Set the platform base
+        self.mp_pl_platform_base = self.config_platform
+        logger.info(f"Platform base: {self.mp_pl_platform_base}")
+
 
         # Retrieve configurations with possibility to override via kwargs
         self.mv_data_dfname1 = kwargs.get('mv_data_dfname1', None)
@@ -27,35 +34,38 @@ class CMqlEnvGlobal:
         self.mp_data_filename1 = kwargs.get('mp_data_filename1', None)
         self.mp_data_filename2 = kwargs.get('mp_data_filename2', None)
 
-        # Determine the platform and set the base directory accordingly
-        self.config_platform = config.get("default_platform", None)
-        platform_map = {
-            "Windows": 'equinrun',
-            "Linux": 'equinrunlin',
-            "Darwin": 'equinrunmac',
-            "MacOS": 'equinrunmac'
-        }
-        self.mp_pl_platform_base = platform_map.get(self.config_platform, 'equinrun')
-
-        logger.info(f"TuneParams: Platform base: {self.mp_pl_platform_base}")
-
         # Use pathlib for cross-platform path handling
+        # Set the home directory
         self.home_dir = Path.home()
+
         self.drive = kwargs.get('drive', config.get('drive', self.home_dir.anchor))
         self.user = kwargs.get('user', config.get('user', self.home_dir.name))
-        self.project = kwargs.get('project', config.get('project', '8.0 Projects'))
-        self.projectmodel = kwargs.get('projectmodel', config.get('projectmodel', '8.3 ProjectModelsEquinox'))
-        self.projectmodelname = kwargs.get('projectmodelname', config.get('projectmodelname', self.mp_pl_platform_base))
-        self.platformdir = kwargs.get('platformdir', config.get('platformdir', 'Mql5Data'))
+        self.dir1 = kwargs.get('dir1', config.get('dir1', '8.0 Projects'))
+        self.dir2 = kwargs.get('dir2', config.get('dir2', '8.3 ProjectModelsEquinox'))
+        self.platform_dir = kwargs.get('platform_dir', config.get('platform_dir', self.mp_pl_platform_base))
+        # set the datdir
+        self.datadir=kwargs.get('datadir', config.get('datadir', 'Mql5Data')) # Override if needed
 
-        # Construct the data path using pathlib
-        self.mp_data_path = Path(self.drive) / self.user / self.project / self.projectmodel / self.projectmodelname / self.platformdir
+        # Machine learning platform directory
+        self.mp_ml_src_lib = kwargs.get('mp_ml_src_lib', config.get('mp_ml_src_lib', 'PythonLib')) # Machine learning source library
+        self.mp_ml_src_modeldata = kwargs.get('mp_ml_src_modeldata', config.get('mp_ml_src_modeldata', 'tsModelData'))
+        self.ml_directory = kwargs.get('ml_directory', config.get('ml_directory', 'tshybrid_ensemble_tuning_prod')) # Machine learning directory
+        self.ml_model_name = kwargs.get('ml_project_name', config.get('ml_project_name', 'prjEquinox1_prod.keras')) # Machine learning project name
+        self.ml_baseuniq = kwargs.get('ml_baseuniq', config.get('ml_baseuniq', '1')) # Unique identifier as string
+
+       # data directory
+        self.mp_data_dir = kwargs.get('mp_data_dir', config.get('data_dir', 'Data'))
+        
+       # Construct the data path using pathlib
+        self.mp_data_path = Path(self.drive) / self.user / self.dir1 / self.dir2 / self.platform_dir / self.mp_data_dir 
         logger.info(f"Data path: {self.mp_data_path}")
 
+        self.modeldatapath = self.mp_data_path
+
         # ML file paths, use configurations from config.yaml
-        self.mp_ml_src_base = Path(self.drive) / self.user / self.project
+        self.mp_ml_src_base = Path(self.drive) / self.user / self.dir1 / self.dir2
         self.mp_ml_src_lib = Path(config.get('mp_ml_src_lib', 'PythonLib'))
-        self.mp_ml_src_data = Path(config.get('mp_ml_src_data', 'tsModelData'))
+        self.mp_ml_src_modeldata = Path(config.get('mp_ml_src_modeldata', 'tsModelData'))
         self.mp_ml_directory = Path(config.get('mp_ml_directory', 'tshybrid_ensemble_tuning_prod'))
         self.mp_ml_project_name = Path(config.get('mp_ml_project_name', 'prjEquinox1_prod.keras'))
         self.mp_ml_baseuniq = config.get('mp_ml_baseuniq', '1')  # Unique identifier as string
@@ -63,18 +73,27 @@ class CMqlEnvGlobal:
         logger.info(f"TuneParams: Source base: {self.mp_ml_src_base}")
         logger.info(f"TuneParams: Platform base: {self.mp_pl_platform_base}")
         logger.info(f"TuneParams: Source lib: {self.mp_ml_src_lib}")
-        logger.info(f"TuneParams: Source data: {self.mp_ml_src_data}")
+        logger.info(f"TuneParams: Source data: {self.mp_ml_src_modeldata}")
         logger.info(f"TuneParams: Directory: {self.mp_ml_directory}")
         logger.info(f"TuneParams: Project name: {self.mp_ml_project_name}")
         logger.info(f"TuneParams: Baseuniq: {self.mp_ml_baseuniq}")
 
-        self.mp_ml_def_base_path = self.mp_ml_src_base / self.mp_pl_platform_base / self.mp_ml_src_lib / self.mp_ml_src_data / self.mp_ml_directory
+        self.mp_ml_def_base_path = self.mp_ml_src_base / self.mp_pl_platform_base / self.mp_ml_src_lib / self.mp_ml_src_modeldata / self.mp_ml_directory
         self.mp_ml_num_base_path = self.mp_ml_def_base_path / self.mp_ml_baseuniq
         self.mp_ml_checkpoint_filepath = self.mp_ml_def_base_path
 
         logger.info(f"Default base path: {self.mp_ml_def_base_path}")
         logger.info(f"Numbered base path: {self.mp_ml_num_base_path}")
         logger.info(f"Checkpoint file path: {self.mp_ml_checkpoint_filepath}")
+
+        # MQL base path
+        self.mp_mql_dir1 = config.get('mp_mql_dir1', 'Mql5') 
+        
+        self.mp_mql_basepath = Path(self.drive) / self.user / self.dir1 / self.dir2 / self.platform_dir / self.mp_mql_dir1
+        self.mp_mql_data_path = self.mp_data_path
+        logger.info(f"MQL base path: {self.mp_mql_basepath}")
+        logger.info(f"MQL data path: {self.mp_mql_data_path}")
+
 
     def get_params(self):
         return self.__dict__
@@ -88,7 +107,6 @@ class CMqlEnvGlobal:
         # Initialize data environment
         self.data_environments = {
             "dataenv": CMqlEnvData(
-                globalenv=self.gen_environments["globalenv"]
             )
         }
 
@@ -99,16 +117,12 @@ class CMqlEnvGlobal:
 
         # Initialize tuner environment
         self.tuner_environments = {
-            "tuneenv": CMqlEnvTuneML(globalenv=self.gen_environments["globalenv"])
+            "tuneenv": CMqlEnvTuneML()
         }
 
         # Initialize model environment
         self.model_environments = {
-            "modelenv": CMdtunerHyperModel(
-                globalenv=self.gen_environments["globalenv"],
-                mlenv=self.mlearn_environments["mlenv"],
-                tuneenv=self.tuner_environments["tuneenv"],
-            )
+            "modelenv": CMdtunerHyperModel()
         }
 
         # Retrieve parameters safely
