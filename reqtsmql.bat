cfb9fe011b43596e4f7a0d41ad3b90150fcced2d
T Shepherd <shep.antony@gmail.com>
2025-04-06 22:58:21 +0100

    runbase

--- reqtsmql.bat
+++ reqtsmql.bat
@@ -1,17 +1,17 @@
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlappparams
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlbaseparams==2.6.14
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlconnect==2.5.37
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqldataLoader==2.6.24
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqldataparams==2.5.28
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqldataprocess==2.7.59
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlenvcore==2.5.14
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlenvmgr==2.7.16
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlmlparams==2.5.8
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlmlprocess==2.7.55
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlmltuner==2.4.73
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlmltunerparams==2.5.25
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqloverrides==2.7.32
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlplatform==2.3.29
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlreference==2.3.26
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlsetup==2.4.1
-py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgradetsmqlutilities==2.8.29
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlappparams
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlbaseparams==2.6.14
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlconnect==2.5.37
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqldataLoader==2.6.24
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqldataparams==2.5.28
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqldataprocess==2.7.59
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlenvcore==2.5.14
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlenvmgr==2.7.16
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlmlparams==2.5.8
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlmlprocess==2.7.55
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlmltuner==2.4.73
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlmltunerparams==2.5.25
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqloverrides==2.7.32
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlplatform==2.3.29
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlreference==2.3.26
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlsetup==2.4.1
+py -m pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade tsmqlutilities==2.8.29
