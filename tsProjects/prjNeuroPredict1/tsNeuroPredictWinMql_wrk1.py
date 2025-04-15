b9c338a9f3cad993fb649a4943377b38269a8213
T Shepherd <shep.antony@gmail.com>
2025-04-15 09:48:01 +0100

    adding chief worker model

--- tsProjects/prjNeuroPredict1/tsNeuroPredictWinMql_wrk1.py
+++ tsProjects/prjNeuroPredict1/tsNeuroPredictWinMql_wrk1.py
@@ -9,7 +9,7 @@
 # property version   "1.01"
 # +------------------------------------------------------------------+
 import os
-tuner_id = 'chief' # chief for master the tuner01, worker
+tuner_id = 'tuner1' # chief for master the tuner01, worker
 # Set the Keras parallelism environment variables Chief
 os.environ["KERASTUNER_TUNER_ID"] = tuner_id
 os.environ["KERASTUNER_ORACLE_IP"] = "127.0.0.1"
