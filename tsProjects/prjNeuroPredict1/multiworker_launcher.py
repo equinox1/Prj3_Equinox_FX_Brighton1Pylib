cf06a3de300c47e6c0ee3dc5bc2130f07deb80f4
tshepherd <shep.antony@gmail.com>
2025-05-17 14:15:28 +0100

    workingmodel tensor and torchmaster2

--- tsProjects/prjNeuroPredict1/multiworker_launcher.py
+++ tsProjects/prjNeuroPredict1/multiworker_launcher.py
@@ -10,7 +10,7 @@ import logging
 from tsMqlSetup import CMqlSetup
 
 # ==== CONFIGURATION ====
-NUM_WORKERS = 4
+NUM_WORKERS = 1
 PYTHON_EXEC = r"C:\WinRunMnt1\8.0 Projects\8.3 ProjectModelsEquinox\EQUINRUN\PythonLib\.venv\Scripts\python.exe"
 BASE_PATH = r"C:/WinRunMnt1/8.0 Projects/8.3 ProjectModelsEquinox/EQUINRUN/PythonLib"
 
--- tsPyPackages/tsMqlDataLoader/tsMqlDataLoader.py
+++ tsPyPackages/tsMqlDataLoader/tsMqlDataLoader.py
@@ -17,7 +17,44 @@ from datetime import datetime
 from tsMqlPlatform import run_platform, platform_checker, get_config
 from tsMqlEnvMgr import CMqlEnvMgr
 import logging
-logger = logging.getLogger(__name__)
+
+
+# -- start of logging setup --
+from tsMqlSetup import CMqlSetup
+from tsMqlOverrides import CMqlOverrides
+
+env_backend = os.environ.get("MLTUNE_BACKEND", "tensorflow")
+env_gtuner = os.environ.get("GTUNER_MODEL", env_backend)
+
+mql_overrides = CMqlOverrides()
+mql_overrides.env.override_params({
+    "mltune": {"backend": env_backend},
+    "app": {"gtuner_model": env_gtuner}
+})
+
+app_params = mql_overrides.env.all_params().get("app", {})
+gtuner_model = app_params.get('gtuner_model', 'pytorch')
+xerces_servername = app_params.get('xerces_servername', "WINSVRXERCES01")
+xerces_server = app_params.get('xerces_server', '192.168.1.103')
+xerces_logfile = app_params.get('xerces_logfile', 'tsneuropredict_app.log')
+
+setup_config = CMqlSetup(
+    loglevel='INFO',
+    warn='ignore',
+    precision='mixed_bfloat16',
+    tfdebug=False,
+    num_cores=8,
+    num_threads=1
+)
+
+global_logdir, global_logfile = setup_config.set_log_dir(
+    logdir=None,
+    logfile=xerces_logfile,
+    servername=xerces_servername,
+    ltuner=gtuner_model
+)
+
+logger = setup_config.setup_global_logger(global_logfile, force_reset=True)
 
 class CDataLoader:
     """Class to manage and load market data with override capability."""
