2d3f12ef6860133f2235aaca35e2e4ee9eb2e0ed
equinox1 <shep.antony@gmail.com>
2025-07-18 23:16:52 +0100

    base6

--- tsProjects/prjNeuroPredict1/multiworker_launcher.py
+++ tsProjects/prjNeuroPredict1/multiworker_launcher.py
@@ -1,6 +1,6 @@
 # filename: multiworker_launcher.py
 # Rewritten: patched_oracle_server_main.py
-runtuner= 'tf' # 'pt' for PyTorch, 'tf' for TensorFlow
+runtuner= 'pt' # 'pt' for PyTorch, 'tf' for TensorFlow
 import os
 import sys
 import time
